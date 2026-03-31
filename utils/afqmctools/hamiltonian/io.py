bbcf22ede1dcbd326817eaf5c08a487a73ba89f2
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2026-03-31 15:43:24 -0400

    afqmctools: fix typos

--- utils/afqmctools/hamiltonian/io.py
+++ utils/afqmctools/hamiltonian/io.py
@@ -12,7 +12,7 @@ import h5py as h5
 import numpy
 from afqmctools.utils.io import to_complex
 
-# TODO: some parameter names are highly mislreading. ex: "real_chol" usually refers to 
+# TODO: some parameter names are highly misleading. ex: "real_chol" usually refers to
 #         whether the one-body terms are real valued (vs. the Cholesky vectors as the name implies)
 
 def write_sparse(hcore, chol, nelec, nmo, e0=0.0, filename='hamiltonian.h5',
