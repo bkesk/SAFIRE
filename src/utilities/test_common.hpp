149ecb8c4c8bf66d3f282128f4b41bd6b5851039
Brandon Eskridge <8075511+bkesk@users.noreply.github.com>
2026-04-30 15:18:24 -0400

    Fixes a merge error

--- src/utilities/test_common.hpp
+++ src/utilities/test_common.hpp
@@ -76,10 +76,10 @@ inline constexpr auto molecule_unit_tests_files(bool rhf, bool uhf, bool ghf, bo
                                           pre + "BH/afqmc_inputs/afqmc_ghf_nomsd.h5",
                                           afqmc::NONCOLLINEAR) );
       files.emplace_back( std::make_tuple(pre + "Pb/afqmc_inputs/afqmc_H_rhf_basis_noncollinear_sf.h5",
-                                          pre + "Pb/afqmc_inputs/afqmc_ghf_sf_nomsd.h5") );
+                                          pre + "Pb/afqmc_inputs/afqmc_ghf_sf_nomsd.h5",
                                           afqmc::NONCOLLINEAR) ); // uncomment when unit test system fixes are merged
       files.emplace_back( std::make_tuple(pre + "Pb/afqmc_inputs/afqmc_H_rhf_basis_noncollinear_soc.h5",
-                                          pre + "Pb/afqmc_inputs/afqmc_ghf_soc_nomsd.h5") );
+                                          pre + "Pb/afqmc_inputs/afqmc_ghf_soc_nomsd.h5",
                                           afqmc::NONCOLLINEAR) ); // uncomment when unit test system fixes are merged
 
     }
