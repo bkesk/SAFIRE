6e54957abf9b7eb092789b0d54dd1d18ff4c2db9
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2026-05-14 11:55:59 -0400

    c++/tests: reactivate commented test_estimators

--- src/AFQMC/Estimators/tests/test_estimators.cpp
+++ src/AFQMC/Estimators/tests/test_estimators.cpp
@@ -19,41 +19,36 @@
 #include "catch2/catch.hpp"
 
 #include "config.h"
-    
+
 #include "IO/ptree/ptree_utilities.hpp"
 #include "utilities/Random.hpp"
 #include "utilities/check.hpp"
-#include "IO/app_loggers.h"
 #include "utilities/test_common.hpp"
-  
+#include "utilities/h5_utils.hpp"
+#include "IO/app_loggers.h"
+
 #include "nda/nda.hpp"
 #include "nda/tensor.hpp"
 #include "nda/h5.hpp"
-#include "numerics/sparse/sparse.hpp"
-    
+
 #include <string>
 #include <vector>
-#include <complex> 
-#include <iomanip>
-#include <random>
+#include <complex>
+#include <format>
+#include <fstream>
 
 #include "AFQMC/config.h"
 #include "AFQMC/Hamiltonians/HamiltonianFactory.h"
 #include "AFQMC/Wavefunctions/WavefunctionFactory.h"
 #include "AFQMC/Walkers/WalkerSetFactory.hpp"
 #include "AFQMC/Estimators/EstimatorBase.h"
+#include "AFQMC/Estimators/BackPropagatedEstimator.hpp"
 #include "AFQMC/Propagators/PropagatorFactory.h"
-//#include "AFQMC/Estimators/BackPropagatedEstimator.hpp"
 #include "AFQMC/Utilities/readWfn.h"
 #include "AFQMC/Utilities/test_utils.hpp"
 #include "AFQMC/Utilities/AFQMCTimer.h"
 
-using std::cerr;
 using std::complex;
-using std::cout;
-using std::endl;
-using std::ifstream;
-using std::setprecision;
 using std::string;
 
 extern std::string UTEST_HAMIL, UTEST_WFN;
@@ -62,18 +57,78 @@ namespace sfqmc
 {
 using namespace afqmc;
 
+namespace
+{
+inline std::string test_hdf_name(std::string const& wfn_file,
+                                 std::string const& hamil_file,
+                                 std::string const& suffix)
+{
+  auto stem = [](std::string const& p) {
+    auto s = p.find_last_of("\\/");
+    auto e = p.find_last_of(".");
+    return p.substr(s + 1, e - s - 1);
+  };
+  return stem(wfn_file) + "_" + stem(hamil_file) + "_" + suffix + ".h5";
+}
+
+template<MEMORY_SPACE MEM>
+void verify_bp_matches_mixed(string const& file, string const& avg_path, int iblock,
+                             WALKER_TYPES type, int NMO, int nup, int ndown,
+                             Wavefunction<MEM>& wfn, WalkerSet<MEM>& wset)
+{
+  int nspin = (type == COLLINEAR) ? 2 : 1;
+  int npol  = (type == NONCOLLINEAR) ? 2 : 1;
+
+  std::string suffix = std::format("{:09d}", iblock);
+
+  nda::array<ComplexType, 1> read_data;
+  ComplexType denom;
+  {
+    h5::file reader(file, 'r');
+    h5::group root(reader);
+    utils::h5_read(root, avg_path + "/one_rdm_" + suffix, read_data);
+    h5::read(root, avg_path + "/denominator_" + suffix, denom);
+  }
+
+  // Since no back propagation has been performed (dt=0), the BP RDM should
+  // equal the mixed estimate.
+  REQUIRE(read_data.size() == nspin * npol * NMO * npol * NMO);
+  auto BPRDM = nda::reshape(read_data, std::array<long, 3>{nspin, npol * NMO, npol * NMO});
+  BPRDM *= 1.0 / denom;
+  ComplexType trace{};
+  for(int spin = 0; spin < nspin; spin++) {
+    for(int i = 0; i < npol * NMO; ++i) {
+      trace += BPRDM(spin, i, i);
+    }
+  }
+  if(type == CLOSED) {
+    trace *= 2;
+  }
+  REQUIRE(trace.real() == Approx(nup + ndown));
+  REQUIRE(trace.imag() == Approx(0.0).margin(1e-9));
+
+  memory::array<MEM, ComplexType, 2> Gw(wset.size(), nspin * npol * NMO * npol * NMO);
+  wfn.MixedDensityMatrix(wset, Gw, false);
+  auto G = nda::reshape(Gw(0,nda::ellipsis{}), std::array<long, 3>{nspin, npol * NMO, npol * NMO});
+  utils::ARRAY_EQUAL(G, BPRDM);
+}
+} // namespace
+
 template<MEMORY_SPACE MEM>
 void reduced_density_matrix(std::shared_ptr<utils::mpi_context_t<boost::mpi3::communicator>> mpi,
              std::string hamil_file, std::string wfn_file)
 {
-  using sfqmc::utils::ARRAY_EQUAL;
-  using nda::range;
+  app_log(1, "Running estimators unit test "
+    "with files:\n --hamil {} \\\n --wfn {}", 
+    hamil_file, wfn_file
+  );
+
   utils::check(utils::file_exists(hamil_file),
                " Hamiltonian file not found: {}. \n Run unit test with --hamil /path/to/hamil.h5 ", hamil_file);
   utils::check(utils::file_exists(wfn_file),
                " Wavefunction file not found: {}. \n Run unit test with --wfn /path/to/wfn.h5 ", wfn_file);
 
-  auto[NMO,nup, ndown] = read_info_from_wfn(UTEST_WFN, "any");
+  auto [NMO, nup, ndown] = read_info_from_wfn(wfn_file, "any");
   utils::check(NMO == read_nmo_from_hdf(hamil_file), "NMO differ between hamil and wfn files.");
 
   std::shared_ptr<utils::RandomGenerator_t<>> rng = std::make_shared<utils::RandomGenerator_t<>>();
@@ -98,8 +153,8 @@ void reduced_density_matrix(std::shared_ptr<utils::mpi_context_t<boost::mpi3::co
   wlk_pt.put("walker_type", walkerTypeToString(type));
   auto wset = make_WalkerSet<MEM>(mpi, wlk_pt, InfoMap["info0"], rng);
 
-  int nspin            = (type == COLLINEAR) ? 2 : 1;
-  int npol             = (type == NONCOLLINEAR) ? 2 : 1;
+  int nspin = (type == COLLINEAR) ? 2 : 1;
+  int npol  = (type == NONCOLLINEAR) ? 2 : 1;
 
   ptree wfn_pt;
   wfn_pt.put("name","wfn0");
@@ -107,7 +162,7 @@ void reduced_density_matrix(std::shared_ptr<utils::mpi_context_t<boost::mpi3::co
   wfn_pt.put("filename",wfn_file);
   wfn_pt.put("dense_trial",true);
 
-  int nwalk = 11; 
+  int nwalk = 11;
   WavefunctionFactory<MEM> WfnFac(InfoMap);
   WfnFac.push("wfn0", wfn_pt);
   auto& wfn = WfnFac.getWavefunction(mpi, "wfn0", type, &ham, nwalk);
@@ -118,217 +173,115 @@ void reduced_density_matrix(std::shared_ptr<utils::mpi_context_t<boost::mpi3::co
 
   PropagatorFactory<MEM> PropgFac(InfoMap);
   PropgFac.push("prop0", prop_pt);
-  PropgFac.getPropagator(mpi, "prop0", wfn, rng_dev);
+  auto& prop = PropgFac.getPropagator(mpi, "prop0", wfn, rng_dev);
 
   auto initial_guess = WfnFac.getInitialGuess("wfn0");
   REQUIRE(initial_guess.shape() == std::array<long,3>{nspin,npol*NMO,nup});
   wset.resize(nwalk, initial_guess);
 
-  using EstimPtr = std::shared_ptr<EstimatorBase<MEM>>;
-  std::vector<EstimPtr> estimators;
-/*
+  // generate P1 with dt=0 so the BP RDM should match the mixed estimate
+  prop.generateP1(0.0, wset.getWalkerType());
+
+  using EstimPtr = std::unique_ptr<EstimatorBase<MEM>>;
+
+  // ---- Run 1: scalar measure_interval_multiplier ----
+  {
     ptree est_pt;
     est_pt.put("name","back_propagation");
-    est_pt.put("nsteps",1); // deprecated
-    est_pt.put("block_size",1); // deprecated
     est_pt.put("measure_interval_multiplier", 2); // test with single value
     est_pt.put("path_restoration","no");
-    est_pt.put("OneRDM.nskip_output",0);
+    est_pt.put("onerdm.nskip_output",0);
     app_log(1,"\nEstimator input:\n{}\n",io::to_string(est_pt));
-    bool impsamp = true;
-    estimators.emplace_back(
-        static_cast<EstimPtr>(std::make_shared<BackPropagatedEstimator>(TG, InfoMap["info0"], "none", est_pt,
-                                                                        type, wset, wfn, prop, impsamp)));
 
-    // generate P1 with dt=0
-    prop.generateP1(0.0, wset.getWalkerType());
+    std::vector<EstimPtr> estimators;
+    estimators.push_back(std::make_unique<BackPropagatedEstimator<MEM>>(
+        mpi, InfoMap["info0"], "none", est_pt, type, wset, wfn, prop, true));
 
-    std::string file = create_test_hdf(UTEST_WFN, UTEST_HAMIL);
+    std::string file = test_hdf_name(UTEST_WFN, UTEST_HAMIL, "run1");
     std::ofstream out;
-    dump.create(file);
-    dump.open(file);
-    for (int iblock = 0; iblock < 10*afqmc::DEFAULT_POPULATION_CONTROL_INTERVAL; iblock++)
     {
-      wset.advanceBPPos();
-      estimators[0]->accumulate_block(iblock*0.01,wset);
-      estimators[0]->print(out, dump, wset);
-    }
-    dump.close();
-    boost::multi::array<ComplexType, 1> read_data(boost::multi::iextensions<1u>{2 * NMO * NMO});
-
-    ComplexType denom;
-    hdf_archive reader;
-    // Read from a particular block.
-    if (!reader.open(file, H5F_ACC_RDONLY))
-      APP_ABORT(" Error opening estimates.h5. ");
-    reader.read(read_data, "Observables/BackPropagated/FullOneRDM/Average_0/one_rdm_000000004");
-    reader.read(denom, "Observables/BackPropagated/FullOneRDM/Average_0/denominator_000000004");
-    // Test EstimatorHandler eventually.
-    //int nup_READ, ndown_READ, NMO_READ, WALKER_TYPE_READ;
-    //reader.read(nup_READ, "Metadata/nup");
-    //REQUIRE(nup_READ==nup);
-    //reader.read(ndown_READ, "Metadata/ndown");
-    //REQUIRE(ndown_READ==ndown);
-    //reader.read(NMO_READ, "Metadata/NMO");
-    //REQUIRE(NMO_READ==NMO);
-    //reader.read(WALKER_TYPE_READ, "Metadata/WALKER_TYPE");
-    //REQUIRE(WALKER_TYPE_READ==type);
-    reader.close();
-    // Test the RDM. Since no back propagation has been performed the RDM should be
-    // identical to the mixed estimate.
-    if (type == CLOSED)
-    {
-      REQUIRE(read_data.num_elements() >= NMO * NMO);
-      boost::multi::array_ref<ComplexType, 2> BPRDM(read_data.origin(), {NMO, NMO});
-      ma::scal(1.0 / denom, BPRDM);
-      ComplexType trace = ComplexType(0.0);
-      for (int i = 0; i < NMO; i++)
-        trace += BPRDM[i][i];
-      REQUIRE(trace.real() == Approx(nup));
-      boost::multi::array<ComplexType, 2, Allocator> Gw({1, NMO * NMO}, alloc_);
-      wfn.MixedDensityMatrix(wset, Gw, false, true);
-      boost::multi::array_ref<ComplexType, 2, pointer> G(Gw.origin(), {NMO, NMO});
-      verify_approx(G, BPRDM);
-    }
-    else if (type == COLLINEAR)
-    {
-      REQUIRE(read_data.num_elements() >= 2 * NMO * NMO);
-      boost::multi::array_ref<ComplexType, 3> BPRDM(read_data.origin(), {2, NMO, NMO});
-      ma::scal(1.0 / denom, BPRDM[0]);
-      ma::scal(1.0 / denom, BPRDM[1]);
-      ComplexType trace = ComplexType(0.0);
-      for (int i = 0; i < NMO; i++)
-        trace += BPRDM[0][i][i] + BPRDM[1][i][i];
-      REQUIRE(trace.real() == Approx(nup + ndown));
-      boost::multi::array<ComplexType, 2, Allocator> Gw({1, 2 * NMO * NMO}, alloc_);
-      wfn.MixedDensityMatrix(wset, Gw, false, true);
-      boost::multi::array_ref<ComplexType, 3, pointer> G(Gw.origin(), {2, NMO, NMO});
-      verify_approx(G, BPRDM);
-    }
-    else if (type == FULLYPOLARIZED) // Kyle: be careful here; I'm not sure that I have a reference!
-    {
-      REQUIRE(read_data.num_elements() >= NMO * NMO);
-      boost::multi::array_ref<ComplexType, 2> BPRDM(read_data.origin(), {NMO, NMO});
-      ma::scal(1.0 / denom, BPRDM);
-      ComplexType trace = ComplexType(0.0);
-      for (int i = 0; i < NMO; i++)
-        trace += BPRDM[i][i];
-      REQUIRE(trace.real() == Approx(nup));
-      boost::multi::array<ComplexType, 2, Allocator> Gw({1, NMO * NMO}, alloc_);
-      wfn.MixedDensityMatrix(wset, Gw, false, true);
-      boost::multi::array_ref<ComplexType, 2, pointer> G(Gw.origin(), {NMO, NMO});
-      verify_approx(G, BPRDM);
-    }
-    else
-    {
-      APP_ABORT(" NONCOLLINEAR Wavefunction found.");
+      h5::file h5out(file, 'w');
+      for (int iblock = 0; iblock < 10*afqmc::DEFAULT_POPULATION_CONTROL_INTERVAL; ++iblock)
+      {
+        wset.advanceBPPos();
+        estimators[0]->accumulate_block(iblock*0.01, wset);
+        estimators[0]->print(out, h5out, wset);
+      }
     }
 
+    verify_bp_matches_mixed<MEM>(file,
+        "Observables/BackPropagated/FullOneRDM/Average_0", 5,
+        type, NMO, nup, ndown, wfn, wset);
+  }
+
+  // ---- Run 2: vector measure_interval_multiplier ----
+  {
     ptree est_pt2;
     est_pt2.put("name","back_propagation");
-    est_pt2.put("nsteps",1); // deprecated
-    est_pt2.put("block_size",1); // deprecated
-    
+
     std::vector<int> nback_prop_interval_multipliers = {1, 2, 3};
     ptree temp_tree;
     for (const auto& value : nback_prop_interval_multipliers) {
-        ptree item;
-        item.put("", value); // empty key for the value
-        temp_tree.push_back(std::make_pair("", item));
+      ptree item;
+      item.put("", value);
+      temp_tree.push_back(std::make_pair("", item));
     }
-    est_pt2.add_child("measure_interval_multiplier", temp_tree); // test with a vector
+    est_pt2.add_child("measure_interval_multiplier", temp_tree);
     est_pt2.put("path_restoration","no");
-    est_pt2.put("OneRDM.nskip_output",0);
+    est_pt2.put("onerdm.nskip_output",0);
     app_log(1,"\nEstimator input:\n{}\n",io::to_string(est_pt2));
 
     std::vector<EstimPtr> estimators2;
-    estimators2.emplace_back(
-        static_cast<EstimPtr>(std::make_shared<BackPropagatedEstimator>(TG, InfoMap["info0"], "none", est_pt2,
-                                                                        type, wset, wfn, prop, impsamp)));
+    estimators2.push_back(std::make_unique<BackPropagatedEstimator<MEM>>(
+        mpi, InfoMap["info0"], "none", est_pt2, type, wset, wfn, prop, true));
 
-    dump.open(file);
-    for (int iblock = 0; iblock < 6*afqmc::DEFAULT_POPULATION_CONTROL_INTERVAL; iblock++)
+    std::string file = test_hdf_name(wfn_file, hamil_file, "run2");
+    std::ofstream out;
     {
-      wset.advanceBPPos();
-      estimators2[0]->accumulate_block(iblock*0.01,wset);
-      estimators2[0]->print(out, dump, wset);
-    }
-    dump.close();
-
-    if (!reader.open(file, H5F_ACC_RDONLY))
-      APP_ABORT(" Error opening estimates.h5. ");
-    else
-      app_log(1," Successfully opened {}", file);
-    reader.read(read_data, "Observables/BackPropagated/FullOneRDM/Average_1/one_rdm_000000002");
-    reader.read(denom, "Observables/BackPropagated/FullOneRDM/Average_1/denominator_000000002");
-    reader.close();
-
-    if (type == CLOSED)
-    {
-      REQUIRE(read_data.num_elements() >= NMO * NMO);
-      boost::multi::array_ref<ComplexType, 2> BPRDM(read_data.origin(), {NMO, NMO});
-      ma::scal(1.0 / denom, BPRDM);
-      ComplexType trace = ComplexType(0.0);
-      for (int i = 0; i < NMO; i++)
-        trace += BPRDM[i][i];
-      REQUIRE(trace.real() == Approx(nup));
-      boost::multi::array<ComplexType, 2, Allocator> Gw({1, NMO * NMO}, alloc_);
-      wfn.MixedDensityMatrix(wset, Gw, false, true);
-      boost::multi::array_ref<ComplexType, 2, pointer> G(Gw.origin(), {NMO, NMO});
-      verify_approx(G, BPRDM);
-    }
-    else if (type == COLLINEAR)
-    {
-      REQUIRE(read_data.num_elements() >= 2 * NMO * NMO);
-      boost::multi::array_ref<ComplexType, 3> BPRDM(read_data.origin(), {2, NMO, NMO});
-      ma::scal(1.0 / denom, BPRDM[0]);
-      ma::scal(1.0 / denom, BPRDM[1]);
-      ComplexType trace = ComplexType(0.0);
-      for (int i = 0; i < NMO; i++)
-        trace += BPRDM[0][i][i] + BPRDM[1][i][i];
-      REQUIRE(trace.real() == Approx(nup + ndown));
-      boost::multi::array<ComplexType, 2, Allocator> Gw({1, 2 * NMO * NMO}, alloc_);
-      wfn.MixedDensityMatrix(wset, Gw, false, true);
-      boost::multi::array_ref<ComplexType, 3, pointer> G(Gw.origin(), {2, NMO, NMO});
-      verify_approx(G, BPRDM);
-    }
-    else if (type == FULLYPOLARIZED) // Kyle: be careful here; I'm not sure that I have a reference!
-    {
-      REQUIRE(read_data.num_elements() >= NMO * NMO);
-      boost::multi::array_ref<ComplexType, 2> BPRDM(read_data.origin(), {NMO, NMO});
-      ma::scal(1.0 / denom, BPRDM);
-      ComplexType trace = ComplexType(0.0);
-      for (int i = 0; i < NMO; i++)
-        trace += BPRDM[i][i];
-      REQUIRE(trace.real() == Approx(nup));
-      boost::multi::array<ComplexType, 2, Allocator> Gw({1, NMO * NMO}, alloc_);
-      wfn.MixedDensityMatrix(wset, Gw, false, true);
-      boost::multi::array_ref<ComplexType, 2, pointer> G(Gw.origin(), {NMO, NMO});
-      verify_approx(G, BPRDM);
-    }
-    else
-    {
-      APP_ABORT(" NONCOLLINEAR Wavefunction found.");
+      h5::file h5out(file, 'w');
+      // 6 * pc_interval = 60 steps, with max_nback_prop = 3 * pc_interval = 30
+      // => iblock reaches 2; check Average_1 (multiplier=2) written at iblock=2
+      for (int iblock = 0; iblock < 6*afqmc::DEFAULT_POPULATION_CONTROL_INTERVAL; ++iblock)
+      {
+        wset.advanceBPPos();
+        estimators2[0]->accumulate_block(iblock*0.01, wset);
+        estimators2[0]->print(out, h5out, wset);
+      }
     }
 
-    if (!reader.open(file, H5F_ACC_RDONLY))
-      APP_ABORT(" Error opening estimates.h5. ");
-    reader.read(read_data, "Observables/BackPropagated/FullOneRDM/Average_2/one_rdm_000000002");
-    reader.read(denom, "Observables/BackPropagated/FullOneRDM/Average_2/denominator_000000002");
-    reader.close();
+    verify_bp_matches_mixed<MEM>(file,
+        "Observables/BackPropagated/FullOneRDM/Average_1", 2,
+        type, NMO, nup, ndown, wfn, wset);
   }
-*/
 }
 
 TEST_CASE("reduced_density_matrix", "[estimators]")
 {
   auto& mpi = utils::make_unit_test_mpi_context();
 
-  reduced_density_matrix<HOST_MEMORY>(mpi,UTEST_HAMIL,UTEST_WFN);
-
+  if (UTEST_HAMIL!="" and UTEST_WFN!="") {
+    reduced_density_matrix<HOST_MEMORY>(mpi,UTEST_HAMIL,UTEST_WFN);
 #if defined(ENABLE_DEVICE)
-  reduced_density_matrix<DEVICE_MEMORY>(mpi,UTEST_HAMIL,UTEST_WFN);
+    reduced_density_matrix<DEVICE_MEMORY>(mpi,UTEST_HAMIL,UTEST_WFN);
 #endif
+  } else {
+    app_log(0,"Estimators unit testing. Running standard tests.");
+    auto files = utils::get_unit_tests_files(true,true,true,true,true,true);
+    for( auto f : files ) {
+      try {
+        reduced_density_matrix<HOST_MEMORY>(mpi,std::get<0>(f),std::get<1>(f));
+      } catch (const sfqmc::AppAbortException& e) {
+        FAIL_CHECK("APP_ABORT in reduced_density_matrix<HOST_MEMORY>(" << std::get<0>(f) << "): " << e.what());
+      }
+#if defined(ENABLE_DEVICE)
+      try {
+        reduced_density_matrix<DEVICE_MEMORY>(mpi,std::get<0>(f),std::get<1>(f));
+      } catch (const sfqmc::AppAbortException& e) {
+        FAIL_CHECK("APP_ABORT in reduced_density_matrix<DEVICE_MEMORY>(" << std::get<0>(f) << "): " << e.what());
+      }
+#endif
+    }
+  }
 }
 
 } // namespace sfqmc
