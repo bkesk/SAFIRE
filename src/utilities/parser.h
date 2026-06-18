be2f005d83f4c72b69e5f00c3de0c16693ce3f7b
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2026-06-18 11:07:20 -0400

    c++/AFQMCFactory,WalkerSetBase: improve input parameter parsing

--- src/utilities/parser.h
+++ src/utilities/parser.h
@@ -13,16 +13,56 @@
 
 #pragma once
 
+#include <algorithm>
 #include <string>
-#include <iostream>
-#include <fstream>
 #include <vector>
+#include <format>
+#include "utilities/check.hpp"
 
 namespace sfqmc {
 namespace utils
 {
 
 std::vector<std::string> split(std::string const&, std::string const&);
+
+
+template<typename T>
+struct EnumInputSpec {
+  T type{};
+  std::string_view explanation{};
+  std::vector<std::string> names{};
+};
+  
+// consider unifying this together with the other walker functions into a separate file
+template<typename T>
+T parse_enum(std::string_view input, std::initializer_list<EnumInputSpec<T>> specs, std::string_view enum_name) {
+  std::string lower_input{input};
+  std::ranges::transform(lower_input, lower_input.begin(),
+                         [](unsigned char c) { return std::tolower(c); });
+
+  for(const auto& spec : specs) {
+    for(const auto &name: spec.names) {
+      if(lower_input == name) {
+        std::string explanation{};
+        if(!spec.explanation.empty()) {
+          explanation = std::format(" ({})", spec.explanation);
+        }
+        app_log(1,"Using {}{} {}.", spec.names.front(), explanation, enum_name); 
+        return spec.type;
+      }
+    }
+  }
+  std::string allowed;
+  for(const auto& spec : specs) {
+    if(!allowed.empty()) {
+      allowed += ", ";
+    }
+    allowed += std::format("\"{}\"", spec.names.front());
+  }
+    
+  check(false, "Unknown {} type \"{}\" has to be one of {{{}}}.", enum_name, lower_input, allowed);
+  return T{};
+}
 
 }
 }
